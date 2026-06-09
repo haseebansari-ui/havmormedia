1f3aa91ff01fc4abf3b414c0f3938ecf2cdf8a14
haseebansari-ui <haseeb.ansari@havmormedia.com>
2026-06-09 18:02:06 +0530

    working on index page

--- js/script.js
+++ js/script.js
@@ -1,3 +1,17 @@
 "use strict"
 
+// ===============
+// 	Scroll Header
+// ===============
+
+const header = document.querySelector("header");
+window.addEventListener("scroll", () => {
+  if (window.scrollY > 10) {
+    header.classList.add("h-scroll");
+  } else {
+    header.classList.remove("h-scroll");
+  }
+});
+
+
 
